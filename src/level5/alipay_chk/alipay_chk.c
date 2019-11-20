f1f7dd46608e051e98455ee3cf1c8dc0406bc4d9
oun111 <oun111@github.com>
2019-11-20 16:38:51 +0800

    update alipay_chk module

--- src/level5/alipay_chk/alipay_chk.c
+++ src/level5/alipay_chk/alipay_chk.c
@@ -48,6 +48,13 @@ int alipay_chk_parse_response(char *resp, size_t sz)
 {
   int ret = -1;
   char *body = get_http_body_ptr(resp,sz);
+#define READ_RESP(__submap) do {\
+  char *retcode = get_tree_map_value(__submap,"code"); \
+  char *retmsg = get_tree_map_value(__submap,"sub_msg"); \
+  log_info("command %s, message: %s\n",!strcmp(retcode,"10000")?"success":"fail", \
+      retmsg); \
+  ret = 0;\
+} while(0)
 
 
   if (!body) {
@@ -68,20 +75,21 @@ int alipay_chk_parse_response(char *resp, size_t sz)
     goto __done;
   }
 
-  tree_map_t submap = get_tree_map_nest(map,"root");
-  submap = get_tree_map_nest(submap,"alipay_fund_account_query_response");
+  tree_map_t submap1 = get_tree_map_nest(map,"root"), submap = 0;
 
-  char *retcode = get_tree_map_value(submap,"code");
-  char *retmsg = get_tree_map_value(submap,"sub_msg");
+  submap = get_tree_map_nest(submap1,"alipay_fund_account_query_response");
+  if (submap) {
+    READ_RESP(submap); 
+    goto __done ;
+  }
 
-
-  log_info("command %s, message: %s\n",!strcmp(retcode,"10000")?"success":"fail",
-      retmsg);
-
-  ret = 0;
+  submap = get_tree_map_nest(submap1,"alipay_security_risk_customerrisk_query_response");
+  if (submap) {
+    READ_RESP(submap); 
+    goto __done ;
+  }
 
 __done:
-
   if (pr)
     jsons_release(pr);
 
@@ -103,7 +111,7 @@ int alipay_chk_rx(Network_t net, connection_t pconn)
     char *b = dbuffer_ptr(pconn->rxb,0);
     b[sz_in] = '\0' ;
 
-    //log_debug("rx resp: %s",b);
+    log_debug("rx resp: %s",b);
 
     dbuffer_lseek(pconn->rxb,sz_in,SEEK_CUR,0);
 
@@ -283,7 +291,7 @@ __done:
   return ret;
 }
 
-int construct_req_params(tree_map_t pay_data, const char *app_id, 
+int construct_account_query_req_params(tree_map_t pay_data, const char *app_id, 
      const char *priv_key_path)
 {
   char tmp[64] = "";
@@ -326,6 +334,48 @@ int construct_req_params(tree_map_t pay_data, const char *app_id,
   return 0;
 }
 
+int construct_risk_query_req_params(tree_map_t pay_data, const char *app_id, 
+     const char *priv_key_path)
+{
+  char tmp[64] = "";
+  time_t curr = time(NULL);
+  struct tm *tm = localtime(&curr);
+
+
+  snprintf(tmp,sizeof(tmp),"%04d-%02d-%02d %02d:%02d:%02d",tm->tm_year+1900,
+           tm->tm_mon+1,tm->tm_mday,tm->tm_hour,tm->tm_min,
+           tm->tm_sec);
+  put_tree_map_string(pay_data,"timestamp",tmp);
+
+  put_tree_map_string(pay_data,"app_id",(char*)app_id);
+
+  put_tree_map_string(pay_data,"method","alipay.security.risk.customerrisk.query");
+
+  put_tree_map_string(pay_data,"format","JSON");
+
+  put_tree_map_string(pay_data,"charset","utf-8");
+
+  put_tree_map_string(pay_data,"sign_type","RSA2");
+
+  put_tree_map_string(pay_data,"version","1.0");
+
+  tree_map_t pay_biz = new_tree_map();
+  put_tree_map_string(pay_biz,"risk_type","riskinfo_business_license_no_isv,riskinfo_cert_no_isv");
+  put_tree_map_string(pay_biz,"business_license_no","91441300MA52Y7790X");
+
+  dbuffer_t strBiz = create_json_params(pay_biz);
+  put_tree_map_string(pay_data,"biz_content",strBiz);
+
+  drop_dbuffer(strBiz);
+  delete_tree_map(pay_biz);
+
+  if (do_signature(pay_data,priv_key_path)) {
+    return -1;
+  }
+
+  return 0;
+}
+
 static int alipay_chk_biz(const char *app_id, const char *priv_key_path)
 {
   Network_t net = get_current_net();
@@ -341,7 +391,10 @@ static int alipay_chk_biz(const char *app_id, const char *priv_key_path)
   }
 
   pay_data = new_tree_map();
-  construct_req_params(pay_data,app_id,priv_key_path);
+  // 商户风险查询，支付宝没有开放该接口 :(
+  construct_risk_query_req_params(pay_data,app_id,priv_key_path);
+  // 商户可用余额查询，支付宝没有开放该接口 :(
+  //construct_account_query_req_params(pay_data,app_id,priv_key_path);
 
   // send request
   create_http_get_req(&out_conn->txb,url,pt_html,pay_data);
@@ -371,10 +424,10 @@ static int do_check_alipay_chans()
   mch_res_item_t pos,n;
   llist_for_each_entry(pos,res_list.first,upper) {
 
-    char *i = pos->app_id, *p = pos->priv_key_path;
-    log_debug("%s - %s\n",i,p);
+    //char *i = pos->app_id, *p = pos->priv_key_path;
+    //log_debug("%s - %s\n",i,p);
 
-    if (0&& strcmp(i,"2019110568911564"))
+    if (1&& strcmp(pos->app_id,"2019110568911564"))
       continue ;
 
     alipay_chk_biz(pos->app_id,pos->priv_key_path);
